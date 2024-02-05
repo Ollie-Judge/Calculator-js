af0857b98cc1ab248bac4625fdcd6d403c7ff11b
Ollie-Judge <ollie.judge@hotmail.com>
2024-02-05 13:42:32 +0000

    got the operation symbol

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const button = document.getElementsByTagName("button");
 
 const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
 
 numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.innerText);
+  });
+});
+
+operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(button.innerText);
   });
