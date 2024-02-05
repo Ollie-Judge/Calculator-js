60ea9871d9b35d15afebec468c7646cafdc0f962
Ollie-Judge <ollie.judge@hotmail.com>
2024-02-05 13:53:02 +0000

    got the output of all buttons

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const button = document.getElementsByTagName("button");
 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelectorAll("[data-delete]");
+const clearButton = document.querySelectorAll("[data-all-clear]");
+const equalsButton = document.querySelectorAll("[data-equals]");
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -10,6 +13,24 @@ numberButtons.forEach((button) => {
 });
 
 operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.innerText);
+  });
+});
+
+deleteButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.innerText);
+  });
+});
+
+clearButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.innerText);
+  });
+});
+
+equalsButton.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(button.innerText);
   });
